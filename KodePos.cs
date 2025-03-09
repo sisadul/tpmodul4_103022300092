5b496b901fa30c1ca05fa9c850060f3000c58c9b
sisadul <abdulazizsaepurohmat1@gmail.com>
2025-03-09 14:34:47 +0700

    menambahkan implementasi dengan table-driven

---
+++ KodePos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_103022300092
+{
+    class KodePos
+    {
+        public enum Kelurahan 
+        {Batununggal,Kujangsari,Mengger,
+        Wates,Cijaura,Jatisari,Margasari,Sekejati,Kebonwaru,Maleer,Samoja}
+
+        
+
+        public static int getKodePos(Kelurahan kel)
+        {
+            int[] kode = { 40266,40287,40267,40256,40287,40286,40286,40286,40272,
+             40274,40273};
+            return kode[(int)kel];
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace tpmodul4_103022300092
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kelurahan    " + "Kode Pos");
+            Console.WriteLine("Batununggal  " + KodePos.getKodePos(KodePos.Kelurahan.Batununggal));
+            Console.WriteLine("Kujangsari   " + KodePos.getKodePos(KodePos.Kelurahan.Kujangsari));
+            Console.WriteLine("Mengger      " + KodePos.getKodePos(KodePos.Kelurahan.Mengger));
+            Console.WriteLine("Wates        " + KodePos.getKodePos(KodePos.Kelurahan.Wates));
+            Console.WriteLine("Cijaura      " + KodePos.getKodePos(KodePos.Kelurahan.Cijaura));
+            Console.WriteLine("Jatisari     " + KodePos.getKodePos(KodePos.Kelurahan.Jatisari));
+            Console.WriteLine("Margasari    " + KodePos.getKodePos(KodePos.Kelurahan.Margasari));
+            Console.WriteLine("Sekejati     " + KodePos.getKodePos(KodePos.Kelurahan.Sekejati));
+            Console.WriteLine("Kebonwaru    " + KodePos.getKodePos(KodePos.Kelurahan.Kebonwaru));
+            Console.WriteLine("Maleer       " + KodePos.getKodePos(KodePos.Kelurahan.Maleer));
+            Console.WriteLine("Samoja       " + KodePos.getKodePos(KodePos.Kelurahan.Samoja));
+        }
+    }
+}
